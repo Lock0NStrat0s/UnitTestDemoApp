e039c9cc9a0ea2b7834ad41a94b8028be6669bea
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-27 00:26:02 -0600

    Add project files.

---
+++ DemoLibrary/DisplayMessages.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class DisplayMessages
+    {
+        public string Greeting(string firstName, int hourOfTheDay)
+        {
+            string output = "";
+            if (hourOfTheDay < 12)
+            {
+                output = $"Good Morning {firstName}";
+            }
+            else if (hourOfTheDay < 18)
+            {
+                output = $"Good Afternoon {firstName}";
+            }
+            else
+            {
+                output = $"Good Evening {firstName}";
+            }
+
+            return output;
+        }
+    }
+}
